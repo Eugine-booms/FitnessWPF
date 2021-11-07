2298a78932f9930ea4bdf198ce3cb43922527338
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-07 20:04:48 +0300

    отказался от сортировки

--- ViewModel/CurentUserChangeViewModel.cs
+++ ViewModel/CurentUserChangeViewModel.cs
@@ -17,29 +17,7 @@ namespace FitnessWPF.ViewModel
 {
     public class CurentUserChangeViewModel : DependencyObject, INotifyPropertyChanged
     {
-        public string Filtertext
-        {
-            get { return (string)GetValue(FiltertextProperty); }
-            set 
-            { 
-                SetValue(FiltertextProperty, value);
-                OnPropertyChanged("Filtertext");
-            }
-        }
 
-        // Using a DependencyProperty as the backing store for Filtertext.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty FiltertextProperty =
-            DependencyProperty.Register("Filtertext", typeof(string), typeof(CurentUserChangeViewModel), new PropertyMetadata("", FilterText_Changed));
-
-        private static void FilterText_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var curent = d as CurentUserChangeViewModel;
-            if (curent != null)
-            {
-                curent.Users.Filter = null;
-                curent.Users.Filter = curent.FilterUsers;
-            }
-        }
 
         public ICollectionView Users
         {
@@ -51,7 +29,24 @@ namespace FitnessWPF.ViewModel
         public static readonly DependencyProperty ItemsProperty =
             DependencyProperty.Register("Items", typeof(ICollectionView), typeof(CurentUserChangeViewModel), new PropertyMetadata(null));
         //public ObservableCollection<User> Users { get; private set; }
-
+        private string _textBoxLogin;
+        public string TextBoxLogin
+        {
+            get => _textBoxLogin;
+            set
+            {
+                _textBoxLogin = value;
+                OnPropertyChanged("TextBoxLogin");
+                foreach (User item in Users)
+                {
+                    if (item.Name.ToLower().Equals(value.ToLower()))
+                    {
+                        _curentUser = item;
+                        OnPropertyChanged("CurentUser");
+                    }
+                }
+            }
+        }
         private User _curentUser;
         public User CurentUser
         {
@@ -60,6 +55,7 @@ namespace FitnessWPF.ViewModel
             {
                 _curentUser = value;
                 OnPropertyChanged("CurentUser");
+                TextBoxLogin = CurentUser.Name;
             }
 
         }
@@ -69,47 +65,46 @@ namespace FitnessWPF.ViewModel
             //Users = new ObservableCollection<User>(UserManager.LoadUsers());
             EnterUserCommand = new DelegateCommand(EnterUser, CanEnterUser);
             NewUserCommand = new DelegateCommand(NewUser);
-            Users.Filter = FilterUsers;
+            //Users.Filter = FilterUsers;
         }
+        //public string Filtertext
+        //{
+        //    get { return (string)GetValue(FiltertextProperty); }
+        //    set
+        //    {
+        //        SetValue(FiltertextProperty, value);
+        //        OnPropertyChanged("Filtertext");
+        //    }
+        //}
 
-        private bool FilterUsers(object obj)
-        {
-            bool result = true;
-            var curent = obj as User;
-            if (curent!=null  && !curent.Name.Contains(Filtertext))
-            {
-                result = false;
-            }
-            if (CurentUser != null)
-            {
-                Filtertext = CurentUser.Name;
-            }
-            else
-            {
-                foreach (User item in Users)
-                {
-                    if (Filtertext == item.Name)
-                    {
-                        CurentUser = item;
-                        break;
-                    }
-                }
-            }
-           return result;
-        }
+        //// Using a DependencyProperty as the backing store for Filtertext.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty FiltertextProperty =
+        //    DependencyProperty.Register("Filtertext", typeof(string), typeof(CurentUserChangeViewModel), new PropertyMetadata("", FilterText_Changed));
+
+        //private static void FilterText_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    var curent = d as CurentUserChangeViewModel;
+        //    if (curent != null)
+        //    {
+        //        curent.Users.Filter = null;
+        //        curent.Users.Filter = curent.FilterUsers;
+        //    }
+        //}
+        //private bool FilterUsers(object obj)
+        //{
+        //    bool result = true;
+        //    var curent = obj as User;
+        //    if (curent != null && !curent.Name.Contains(Filtertext))
+        //    {
+        //        result = false;
+        //    }
+        //    return result;
+        //}
 
         private bool CanEnterUser(object arg)
         {
             return (arg as User) != null;
         }
-
-        private void NewUser(object obj)
-        {
-            var newUserwindow = new CreateNewUser();
-            var newUserwindowViewModel = new CreateNewUserViewModel();
-            newUserwindow.ShowDialog();
-        }
-
         private void EnterUser(object obg)
         {
             var newUserwindow = new MainWindow();
@@ -117,6 +112,14 @@ namespace FitnessWPF.ViewModel
             newUserwindow.ShowDialog();
 
         }
+        private void NewUser(object obj)
+        {
+            var newUserwindow = new CreateNewUser();
+            var newUserwindowViewModel = new CreateNewUserViewModel();
+            newUserwindow.ShowDialog();
+        }
+
+
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
